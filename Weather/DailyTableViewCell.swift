65f15d17d2b077f29143ddc995615e289cc797ec
rochagi <grocha0507@gmail.com>
2023-12-03 22:46:05 -0300

    table view cell components

--- Weather/DailyTableViewCell.swift
+++ Weather/DailyTableViewCell.swift
@@ -14,17 +14,66 @@ class DailyTableViewCell: UITableViewCell {
     private lazy var weekDayLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        
+        label.text = "ter".uppercased()
+        label.textColor = UIColor.contrastColor
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
         return label
+    }()
+    private lazy var minDayLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "min 25oC"
+        label.textColor = UIColor.contrastColor
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        return label
+    }()
+    private lazy var maxDayLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "max 32oC"
+        label.textColor = UIColor.contrastColor
+        label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        return label
+    }()
+    private lazy var iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "cloudIcon")
+        return imageView
+    }()
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [weekDayLabel, iconImageView, minDayLabel, maxDayLabel])
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
     }()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        backgroundColor = .blue
+        setupView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView(){
+        setHierarchy()
+        setConstraints()
+    }
+    private func setHierarchy(){
+        contentView.addSubview(stackView)
+        
+    }
+    private func setConstraints(){
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        ])
+        
     }
     
 }
