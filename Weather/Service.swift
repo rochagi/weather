d8cd2e6eaa18680930eebeb9aa304b9b30675f21
rochagi <grocha0507@gmail.com>
2023-12-06 23:21:49 -0300

    API

---
+++ Weather/Service.swift
@@ -0,0 +1,14 @@
+//
+//  Service.swift
+//  Weather
+//
+//  Created by Giovanna Rocha on 06/12/23.
+//
+
+import Foundation
+
+class service {
+    private let baseURL: String = "https://api.openweathermap.org/data/3.0/onecall"
+    private let APIKey: String = "2b75a26ac7e3392386bd92fecfad70d3"
+    
+}
